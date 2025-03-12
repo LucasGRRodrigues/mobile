165afea0241e72f897f5c4cbb70a4f7c4d93df1f
Lucas Rodrigues <lucasgabri3l@hotmail.com>
2025-03-11 21:58:11 -0300

    subindo tudo

---
+++ App.js
@@ -0,0 +1,12 @@
+import Flexbox from "./screens/Flexbox";
+import GaleriaScreen from "./screens/GaleriaScrenn";
+import PadraoAndroid from "./screens/PadraoAndroid";
+import PadraoIOS from "./screens/PadraoIOS";
+
+function App() {
+    return (
+        <GaleriaScreen/>
+    );
+}
+
+export default App;
